98f054b13c5b2fc3552a8335adb6618b934a7670
Arnon Paiva <arnongp@gmail.com>
2023-04-30 17:53:33 -0300

    fix: ajuste logica js

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-const form = document.getElementById('form-valida')
-var formularioEValido
+const form = document.getElementById('form-numero')
+const campoA = document.getElementById('campoA')
+const campoB = document.getElementById('campoB')
 
-function validaFormulario(campo1, campo2) {
-  let numero1 = parseInt(campo1)
-  let numero2 = parseInt(campo2)
-  formularioEValido = numero2 > numero1
-  return formularioEValido
+let formEValido = false
+
+function validaForm(numero2, numero1) {
+  return parseInt(numero2) > parseInt(numero1)
 }
-
-validaFormulario(41, 121)
 
 form.addEventListener('submit', function (e) {
   e.preventDefault()
+})
 
-  if (formularioEValido) {
-    document.querySelector('#message').innerHTML = 'Formulário Válido'
-    console.log('Formulário Válido')
-    campoA.value = ''
-    campoB.value = ''
-  } else {
-    document.querySelector('#message').innerHTML = 'Formulário Inválido'
-    console.log('Formulário Inválido')
-    campoA.value = ''
-    campoB.value = ''
-  }
+campoB.addEventListener('keyup', function (e) {
+  console.log(e.target.value)
+  console.log(campoA.value)
+
+  console.log(validaForm(130, 120))
+  console.log(campoA.value)
 })
